755c461cbfc2931f2dfb90ef182ce9ec6a11038e
kafu77 <kafubere99@gmail.com>
2024-09-01 19:32:52 -0700

    First setup.py & requirements.txt

--- setup.py
+++ setup.py
@@ -3,27 +3,25 @@ from typing import List
 
 HYPHEN_E_DOT ="-e ."
 
-def get_requirements(file_path: str)-> list[str]:
-    
+def get_requirements(file_path: str)-> List[str]:
     """
     This function will return a list of strings
     """
     requirements = []
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
+    with open(file_path) as file_object:
+        requirements=file_object.readlines()
         requirements=[req.replace("\n","") for req in requirements]
         
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
-        return requirements
+    return requirements
 
 setup(
     name="MLProject",
-    version="0.0.1",
+    version="0",
     author="Kafu",
     author_email="kafubere99@gmail.com",
+    url="https://github.com/kafubere99/MLProject",
     packages=find_packages(),
-    install_requires=get_requirements("requirements.txt"),
-    
-    
-)
+    install_requires=get_requirements("requirements.txt")
+    )
